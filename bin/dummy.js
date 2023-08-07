756dfbecdfa09d3ff0a350b6223fa4e968f2fb0a
mklee-mac <mklee@headit.co.kr>
2023-08-07 23:33:21 +0900

    rm husky

--- bin/dummy.js
+++ bin/dummy.js
@@ -1,7 +1,40 @@
 #!/usr/bin/env node
-const { program } = require("commander");
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
+const commander = require('commander');
+const program = new commander.Command();
+const jsonfile = require('jsonfile');
+const { ApiCaller } = require("../utils/api-caller");
+require('../utils/api-caller');
+
+program
+  .name('dummy data generator')
+  .description('dummy data generator')
+  .version('0.0.1');
+
+program
+  .command('g')
+  .description('Generate dummy data')
+  .requiredOption('-f, --file <path>', 'import data config file path')
+  .option('-o, --output <type>', 'Output type (json or xml)', 'json')
+  .action(async (options) => {
+
+    try {
+      const config = jsonfile.readFile(options.file);
+      const client = ApiCaller.createClient('token');
+      const result = await client.generateDummyData(config, options.output, 10);
+    
+      console.log(`result: ${JSON.stringify(result)}`);
+      const outputPath = `./default.${options.output}`;
+      jsonfile.writeFile(outputPath, result);
+      console.log(`Dummy data saved to: ${outputPath}`);
+    } 
+    catch (error) {
+      console.error('Error:', error.message);
+    }
+  });
+
+
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
---
+++ utils/api-caller.ts
@@ -0,0 +1,29 @@
+
+export class ApiCaller {
+  
+  static createClient(token: string):  ApiCaller {
+    return new ApiCaller();
+  };
+  
+  generateDummyData(config: any, output: OutputType, count: number): Promise<any> {
+    return Promise.resolve([
+      {
+        name: '일민규', 
+        email: 'mk1@email.com'
+      },
+      {
+        name: '이민규', 
+        email: 'mk2@email.com'
+      },
+      {
+        name: '삼민규', 
+        email: 'mk3@email.com'
+      }
+    ]);
+  }
+}
+
+export enum OutputType {
+  JSON = 'json', 
+  XML = 'xml'
+}
