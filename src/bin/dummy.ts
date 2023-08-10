e1cfb75bb8535dd56a227c4fe16977431fc593bd
STEVE <ymyd.steve@gmail.com>
2023-08-10 22:31:27 +0900

    fix : delete some file

--- src/bin/dummy.ts
+++ src/bin/dummy.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import { GenerateCommand } from "../commands";
+import { GenerateCommand } from '../commands';
+import { BANNER } from '../ui';
 
-const initCommand = new GenerateCommand
+const initCommand = new GenerateCommand();
 
+console.log(BANNER);
 initCommand.load();
