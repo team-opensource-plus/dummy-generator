58ac43d68868d82f13f19a6d9800e2784c184403
mklee-mac <mklee@headit.co.kr>
2023-08-10 22:23:06 +0900

    필요없는 파일 삭제 중 - build 후 생성된 js 파일 삭제

--- src/gptapi/api-caller.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class ApiCaller {
-    static createClient(token: string): ApiCaller;
-    generateDummyData(config: any, output: OutputType, count: number): Promise<any>;
-}
-export declare const apiCaller: (token: any) => any;
-export declare enum OutputType {
-    JSON = "json",
-    XML = "xml"
-}
--- src/gptapi/index.ts
+++
@@ -1,6 +0,0 @@
-import 'reflect-metadata';
-export { ApiCaller, apiCaller, OutputType } from './api-caller';
-export { } from './export-function';
-
-
-import axios from 'axios'
