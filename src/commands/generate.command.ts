f93f855dc85091a6c87ae8f9f17d6348d7abb0e7
STEVE <ymyd.steve@gmail.com>
2023-08-14 19:02:02 +0900

    fix : gpt error

--- src/commands/generate.command.ts
+++ src/commands/generate.command.ts
@@ -2,6 +2,8 @@ import { Command } from 'commander';
 import { Configuration, OpenAIApi } from 'openai';
 import { ApiCaller, OutputType } from '../gptapi';
 import * as jsonfile from 'jsonfile';
+import * as path from 'path';
+import * as fs from 'fs';
 
 export class GenerateCommand {
   private program;
@@ -23,22 +25,24 @@ export class GenerateCommand {
       .action(async (options: any) => {
         try {
           console.log(`-f : ${options.file}`);
+          validateConfigFile(options.file);
           const config = await jsonfile.readFileSync(options.file);
+
           console.log(`config: ${JSON.stringify(config)}`);
+
           const client = new ApiCaller();
 
-          const result = await client.createChatCompletion(
+          await client.createChatCompletion(
             config,
             OutputType.JSON,
             10,
           );
-          console.log(client.prompt);
-          const result2 = await client.callGptApi();
-          console.log(result2)
-          // console.log(`result: ${JSON.stringify(result2)}`);
+          const result = await client.callGptApi();
 
           const outputPath = `./default.${options.output}`;
-          await jsonfile.writeFileSync(outputPath, result2);
+
+          await jsonfile.writeFileSync(outputPath, result);
+
           console.log(`Dummy data saved to: ${outputPath}`);
         } catch (error: any) {
           console.error('Error:', error.message);
@@ -46,4 +50,45 @@ export class GenerateCommand {
       })
       .parse(process.argv);
   }
+
+}
+
+export function validateConfigFile(filePath: string): any {
+  const configFilePath = path.resolve(filePath);
+  console.log(`configFilePath : ${configFilePath}`);
+  const data = JSON.parse(fs.readFileSync(configFilePath, 'utf-8'));
+
+  const outputType = data['output_type'];
+  console.log("output_type" + outputType)
+  if (outputType == undefined ||
+    (outputType !== 'json' && outputType !== 'xml' && outputType !== "csv")) {
+    throw new Error('Invalid outputType');
+  }
+
+  const requireCount = data['require_count'];
+  if (requireCount == undefined || requireCount <= 0) {
+    throw new Error('Invalid requireCount');
+  }
+
+  const columns = data.columns;
+  columns.forEach((column: { [x: string]: any; }) => {
+    const columnName = column['column-name'];
+    const columnDescription = column['column-description'];
+    const maxLength = column['max-length'];
+    const isUnique = column['unique'];
+
+    console.log(`Column Name: ${columnName}`);
+    console.log(`Column Description: ${columnDescription}`);
+    console.log(`Max Length: ${maxLength}`);
+    console.log(`Unique: ${isUnique}`);
+    console.log('---');
+
+    if (columnName === undefined || columnDescription === undefined ||
+      maxLength === undefined || isUnique === undefined) {
+      throw new Error('Invalid config file');
+    }
+  });
+
+  console.log(`columns : ${columns}`);
+  return columns;
 }
